4162897659aad9cedf11b934429c8cb1ffc285c9
jeevanuday <jeevanuday07@gmail.com>
2024-05-28 17:17:56 +0530

    Code push for jenkins github

---
+++ HelloWorld.rb
@@ -0,0 +1,12 @@
+class Test 
+    def initialize
+     puts "Code from github"
+     for(i=0; i<10; i++){
+        puts ("Alert:" + i);
+     }
+     puts "Code ended from github"
+    end
+  end
+  
+  # initialize object
+  Test.new
